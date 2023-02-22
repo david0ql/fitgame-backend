0032bd6f13ae28509c0296526f0162c5752f925f
David Gómez <davidgggomez0421@gmail.com>
2023-02-21 20:40:01 -0500

    score added

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-var bodyParser = require('body-parser');
-const { router } = require('./routes/games');
-const { records } = require('./routes/records');
-const { historiales } = require('./routes/historial');
-const { login } = require('./routes/login');
+const cors = require("cors");
+var bodyParser = require("body-parser");
+const { router } = require("./routes/games");
+const { records } = require("./routes/records");
+const { historiales } = require("./routes/historial");
+const { login } = require("./routes/login");
+const { score } = require("./routes/score");
 
-app.use(cors())
-    .use(express.json())
-    .use(express.urlencoded({ extended: true }))
-    .use(bodyParser.json())
-    .use('/games', router)
-    .use('/records', records)
-    .use('/historial', historiales)
-    .use('/login', login)
-    .listen(1111);
-
+app
+  .use(cors())
+  .use(express.json())
+  .use(express.urlencoded({ extended: true }))
+  .use(bodyParser.json())
+  .use("/games", router)
+  .use("/records", records)
+  .use("/historial", historiales)
+  .use("/login", login)
+  .use("/setscore", score)
+  .listen(1111);
